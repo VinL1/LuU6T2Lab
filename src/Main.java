d70830a0b69af2f766edca0d58080b59f3995ced
BT_3W2_04 <vincentl105@nycstudents.net>
2023-01-09 09:06:47 -0500

    Do Now finished

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String [] args) {
+
+        String[] names = {"Jamal", "Emily", "Destiny", "Mateo", "Sofia"};
+
+        // write a for loop to print each name, one per line:
+
+        for (int i = 0; i < names.length; i ++) {
+            System.out.print(names[i] + " ");
+        }
+    }
+}
